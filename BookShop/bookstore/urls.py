533cbe5cc22667f106c193c19fb927928acfefd2
Aza03faza <azamatrahimbek574@gmail.com>
2023-04-12 10:19:05 +0600

    real_madrid
    real_madrid

---
+++ BookShop/bookstore/urls.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+from store.views import UserList,UserRUD
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('cart/', include('cart.urls')),
+    path('search/', include('search.urls')),
+    path('order/', include('order.urls')),
+    path('',include("store.urls")),
+    path('users/',UserList.as_view()),
+    path('user/<int:pk>',UserRUD.as_view()),
+
+   
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
